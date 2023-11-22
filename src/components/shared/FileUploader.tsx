84eb2c2dfff9603f5e2d371afe3a1562fe3f2c16
qixiaa <aqixiaa@gmail.com>
2023-11-23 10:17:42 +1100

    add edit-post

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -9,7 +9,7 @@ type FileUploadProps = {
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploadProps) => {
   const [file, setFile] = useState<File[]>([]);
-  const [fileUrl, setFileUrl] = useState("");
+  const [fileUrl, setFileUrl] = useState(mediaUrl);
 
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
